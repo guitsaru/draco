e761facea8156c7660047b933d641cc8625bfe1b
Matt Pruitt <matt@guitsaru.com>
2021-01-05 20:03:59 +0700

    Fix rubocop

--- lib/draco/benchmark.rb
+++ lib/draco/benchmark.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
+# Public: The Draco ECS library.
 module Draco
-  # Public: Calculates the average time of 
+  # Public: Calculates the average time of
   module Benchmark
     def self.included(mod)
-      mod.prepend(World)
+      mod.prepend(WorldPlugin)
     end
 
-    module World
+    # Internal: The module containing the benchmark plugin for Draco::World.
+    module WorldPlugin
       def before_tick(context)
         @system_timer_data = Hash.new { |h, k| h[k] = [] }
-        super.each { |system| system.class.prepend(System) }
+        super.each { |system| system.class.prepend(SystemPlugin) }
       end
 
       def after_tick(context, results)
@@ -19,15 +21,16 @@ module Draco
           name = Draco.underscore(system.class.name.to_s).to_sym
           @system_timer_data[name] = (system.timer * 1000.0).round(5)
         end
-        puts @system_timer_data.inspect if context.tick_count % 10 == 0
+        puts @system_timer_data.inspect if (context.tick_count % 10).zero?
       end
 
       def system_timers
-        @system_timer_data.sort_by { |k,v| -v }.to_h
+        @system_timer_data.sort_by { |_k, v| -v }.to_h
       end
     end
 
-    module System
+    # Internal: The module containing the benchmark plugin for Draco::System.
+    module SystemPlugin
       def after_initialize
         super
         @timer = 0
